abb88ae1b87cf2a24623ab0d857eed329bc6b26f
Florence <plorenznl@gmail.com>
2024-10-30 22:05:24 +0800

    Count the number if in the correct range.

--- user_input_range_validator.py
+++ user_input_range_validator.py
@@ -15,10 +15,26 @@ while True:
     number_range = input("PLease enter a number ranging from 1 to 50: ")
 
     if number_range.isdigit():
-        number_range = int(number_range.isdigit)
+        number_range = int(number_range)
     else:
         continue
 
-        if 1 <= number_range <= 50:
-            
+    if 1 <= number_range <= 50:
+
+        if 1 <= number_range <= 10:
+            result_1_10 += 1
+        
+        elif 11 <= number_range <= 20:
+            result_11_20 += 1
+
+        elif 21 <= number_range <= 30:
+            result_21_30 += 1
+
+        elif 31 <= number_range <= 40:
+            result_31_40 += 1
+
+        elif 41 <= number_range <= 50:
+            result_41_50 += 1
+    else:
+        break
     
