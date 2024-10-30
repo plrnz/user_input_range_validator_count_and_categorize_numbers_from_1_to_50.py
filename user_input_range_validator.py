b73f60819cbcaa2c57aab9b9d481299745978bab
Florence <plorenznl@gmail.com>
2024-10-30 21:06:58 +0800

    Start the loop

--- user_input_range_validator.py
+++ user_input_range_validator.py
@@ -1,5 +1,9 @@
-# Psudeocodes
+# Pseudocodes
 # 1. Set up the loop and input
 # 2. Check if te input is valid
 # 3. Counting each range
 # 4. Display the results
+
+while True:
+    number_range = int(input("PLease enter a number ranging from 1 to 50: "))
+    
